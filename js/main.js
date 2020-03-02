2fa1a3c106b5e4fa8289888af52be2a1cf1392f9
Aliona_work <alionanovyk99@gmail.com>
2020-03-02 17:27:17 +0200

    fourth lesson: DOM elem

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let start_calc = document.getElementById('start'),
+    budget_value = document.querySelector('.budget-value'),
+    daybudget_value = document.querySelector('.daybudget-value'),
+    level_value = document.querySelector('.level-value'),
+    expenses_value = document.querySelector('.expenses-value'),
+    optionalexpenses_value = document.querySelector('.optionalexpenses-value'),
+    income_value = document.querySelector('.income-value'),
+    monthsavings_value = document.querySelector('.monthsavings-value'),
+    yearsavings_value = document.querySelector('.yearsavings-value'),
+    expenses_item = document.querySelectorAll('.expenses-item'),
+    expenses_item_btn = document.querySelector('button.expenses-item-btn'),
+    optionalexpenses_btn = document.querySelector('button.optionalexpenses-btn'),
+    count_budget_btn = document.querySelector('button.count-budget-btn');
+
+    
